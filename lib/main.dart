30ad2f5ede9a165173054cb2ba933a01ad9cc8c1
garouenh <gggarouneh@gmail.com>
2024-12-16 10:25:04 +0330

    add banner slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_digikala_application/constants/colors.dart';
 import 'package:flutter_digikala_application/widgets/banner_slider.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
